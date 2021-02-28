23e3f6ff9ff203a8f7d42a1e015b027563faab00
Leonardo Rebouças Batista <leo292629@gmail.com>
2021-02-27 22:39:24 -0300

    added content

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,19 @@ function App() {
         Minha Pagina Hospedada no GitHub Pages Com Anúncios do Google
       </div>
       <GoogleAd slot="394738798" timeout={1000} classNames="page-bottom" />
-      <div className="App-header">Meu conteúdo ...</div>
+      <div className="App-header">
+        Em um mês marcado pela volatilidade dos mercados financeiros, a Bolsa
+        teve desempenho negativo, impactada pelo desempenho das ações da
+        Petrobras, assim como o ouro, que recuou. No lado positivo do mercado, o
+        dólar e os fundos imobiliários com cotas negociadas em Bolsa fecharam o
+        período com variações positivas. O aumento dos casos de covid-19 levou
+        governos estaduais e municipais a novas medidas de restrição, enquanto o
+        ritmo da vacinação no país segue prejudicado pela falta de doses. Esse
+        problema já seria suficiente para reduzir o apetite dos investidores por
+        aplicações de risco, dizem profissionais de mercado. Mas para piorar o
+        quadro, a Bolsa ainda teve que enfrentar a desvalorização dos papéis da
+        Petrobras, empresa que representa quase 9% do Ibovespa
+      </div>
     </div>
   );
 }
