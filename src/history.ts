93979a1e8222ff75a86cf8db33b6ae4f738f4f55
Leonardo Rebouças Batista <leo292629@gmail.com>
2021-03-07 16:51:51 -0300

    fix: moved google analytics initialization to index.ts

--- src/history.ts
+++ src/history.ts
@@ -1,14 +1,5 @@
 import { createBrowserHistory } from 'history';
-import ReactGA from 'react-ga';
-
-const trackingId = 'G-Y7W1J25DH6';
-ReactGA.initialize(trackingId);
 
 const history = createBrowserHistory();
-
-history.listen((location) => {
-  ReactGA.set({ page: location.pathname });
-  ReactGA.pageview(location.pathname);
-});
 
 export default history;
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
+import ReactGA from 'react-ga';
 
 import { App } from './components/App';
 import { reducers } from './reducers';
+import history from './history';
 
 declare global {
   interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: typeof compose;
   }
 }
+
+const trackingId = 'G-Y7W1J25DH6';
+ReactGA.initialize(trackingId);
+
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
