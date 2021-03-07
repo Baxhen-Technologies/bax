849e4520373ddce4444d5ca97978e010107f0534
Leonardo Rebouças Batista <leo292629@gmail.com>
2021-03-07 17:25:33 -0300

    feat added input and label for searching on landing

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -7,6 +7,8 @@ export const Landing: React.FC<LandingProps> = () => {
     <div style={{ textAlign: 'center' }}>
       <h1>Município</h1>
       Informações e dados sobre o seu município
+      <label htmlFor="search">Ache seu Município</label>
+      <input id="search" type="text" />
     </div>
   );
 };
