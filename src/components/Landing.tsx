a034e6c4030ba76c468a493eb9ba8a737108e835
Leonardo Rebouças Batista <leo292629@gmail.com>
2021-03-08 16:37:44 -0300

    feat: added list in landing and ad

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { GoogleAd } from './GoogleAd';
 
 interface LandingProps {}
 
@@ -7,8 +9,18 @@ export const Landing: React.FC<LandingProps> = () => {
     <div style={{ textAlign: 'center' }}>
       <h1>Município</h1>
       Informações e dados sobre o seu município
-      <label htmlFor="search">Ache seu Município</label>
-      <input id="search" type="text" />
+      <br />
+      Municípios Disponíveis:
+      <ul className="list-group">
+        <li className="list-group-item">
+          <Link to="/osasco">Osasco</Link>
+        </li>
+        <li className="list-group-item">
+          <Link to="/sao-paulo">São Paulo</Link>
+        </li>
+      </ul>
+      <br />
+      <GoogleAd slot="7259870550" />
     </div>
   );
 };
